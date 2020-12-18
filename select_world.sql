34a1806ff196bfc8376904f6d0a3ee1de2af1529
dibdas <dibyandu08das@gmail.com>
2020-12-19 02:58:04 +0530

    updated select_world.sql
    added more queries

--- select_world.sql
+++ select_world.sql
@@ -42,6 +42,34 @@ SELECT name,population from world WHERE name IN ('France','Germany', 'Italy')
 SELECT name from world WHERE name LIKE('%United%')
 
 
+--10.Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
+--Show the countries that are big by area or big by population. Show name, population and area.
+SELECT name,population,area from world WHERE population > 250000000 OR area >3000000
+
+
+--11.Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
+SELECT name,population,area FROM world WHERE area > 3000000 XOR population > 250000000
 
 
 
+--12.Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+--For South America show population in millions and GDP in billions both to 2 decimal places.
+SELECT name,ROUND(population/1000000,2),ROUND(gdp/1000000000,2) from world WHERE continent='South America'
+
+
+
+--13.Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+--Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+SELECT name,ROUND(gdp/population,-3) from world WHERE gdp >= 1000000000000
+
+
+--14.Show the name and capital where the name and the capital have the same number of characters
+SELECT name,capital FROM world WHERE LENGTH(name) = LENGTH(capital)
+
+
+--15.Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+SELECT name,capital FROM world WHERE name <> capital AND LEFT(name,1) = LEFT(capital,1)
+
+--16.Find the country that has all the vowels and no spaces in its name.
+SELECT name FROM world
+WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %'
