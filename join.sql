dbbb436336b44801014067b1b63a74f93edba843
dibdas <dibyandu08das@gmail.com>
2020-12-30 20:45:16 +0530

    updated join.sql
    added queries

---
+++ join.sql
@@ -0,0 +1,19 @@
+
+--1.Modify it to show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER'
+SELECT matchid,player from goal WHERE teamid='GER'
+
+
+
+--2.Show id, stadium, team1, team2 for just game 1012
+SELECT id,stadium,team1,team2
+  FROM game WHERE id='1012'
+
+
+
+--3.Modify it to show the player, teamid, stadium and mdate for every German goal.
+  SELECT player,teamid,stadium,mdate
+  FROM game JOIN goal ON (game.id=matchid) WHERE teamid ='GER'
+
+
+--4.Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+SELECT team1,team2,player from game JOIN goal ON (game.id=matchid) WHERE player LIKE('Mario%')
