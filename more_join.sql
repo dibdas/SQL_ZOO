de22d3976286c4a55c33e03d9e175edb25a89fcf
dibdas <dibyandu08das@gmail.com>
2021-01-01 14:34:23 +0530

    added more_join.sql

---
+++ more_join.sql
@@ -0,0 +1,10 @@
+
+
+--1.List the films where the yr is 1962 [Show id, title]
+SELECT id, title
+ FROM movie
+ WHERE yr=1962
+
+--2.Give year of 'Citizen Kane'.
+SELECT yr from movie WHERE title = 'Citizen Kane'
+
