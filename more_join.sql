5d5926d132ae52ec9264fec0226722029417d710
dibdas <dibyandu08das@gmail.com>
2021-01-02 01:26:21 +0530

    updated join.sql

--- more_join.sql
+++ more_join.sql
@@ -8,3 +8,14 @@ SELECT id, title
 --2.Give year of 'Citizen Kane'.
 SELECT yr from movie WHERE title = 'Citizen Kane'
 
+
+--3.List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+SELECT id,title,yr from movie WHERE title LIKE  'Star Trek%' ORDER BY yr
+
+
+
+--4.What id number does the actor 'Glenn Close' have?
+SELECT id from actor WHERE name = 'Glenn Close'
+
+--5.What is the id of the film 'Casablanca
+SELECT id from movie WHERE title ='Casablanca'
